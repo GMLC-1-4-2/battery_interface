d3718c0256d998fda4a5b320d0e0fae2b84f193f
texasbb <todd@texasbb.net>
2018-07-04 12:17:32 -0700

    Test commit, just getting started.

--- src/services/peak_managment_service/peak_management_service.py
+++ src/services/peak_managment_service/peak_management_service.py
@@ -13,6 +13,8 @@ from fleet_config import FleetConfig
 
 from fleets.home_ac_fleet.home_ac_fleet import HomeAcFleet
 from fleets.battery_inverter_fleet.battery_inverter_fleet import BatteryInverterFleet
+import numpy as np
+import pandas as pd
 
 
 class PeakManagementService():
@@ -20,10 +22,39 @@ class PeakManagementService():
     This class implements FleetInterface so that it can communicate with a fleet
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, fleet=None, capacity_scaling_factor=1.0, drive_cycle_file="drive.cycle.summer.peaky.csv", *args, **kwargs):
+
+        self.drive_cycle_file = drive_cycle_file
+
+        # Establish a default simulation timestep (in case drive cycle data has no time info?)...
         self.sim_time_step = timedelta(hours=1)
 
-        self.fleet = BatteryInverterFleet()
+        # Set the appropriate fleet to use.
+        # Q:  What's the best way to pass in the fleet to instantiate?  Should the fleet be
+        # instantiated in main and passed in as an object?
+        if fleet == None:
+            self.fleet = BatteryInverterFleet()
+        elif fleet = "HomeAcFleet":
+            self.fleet = HomeAcFleet()
+        else:
+            self.fleet = None   # Need to throw an exception here...
+
+        # Set up the fleet (if necessary?)
+        fleet_config = FleetConfig(is_P_priority=True, is_autonomous=False, autonomous_threshold=None)
+        self.fleet.change_config(fleet_config)
+
+        # Long term, we don't want a drive cycle, we want to call some kind of daily
+        # load forecast service that a higher-level function in the software provides.
+        # But for now, our whole purpose is testing...
+        #
+        # This data frame has columns (year, month, day, hour, load_forecast_mw)
+        self.drive_cycle = pd.read_csv(self.drive_cycle_file)
+        self.drive_cycle.load_forecast_mw /= self.capacity_scaling_factor
+
+        # Ideally the fleet could tell us its capacity and we compute the scaling_factor from that
+        # and the maximum value in the drive cycle...
+        self.capacity_scaling_factor = capacity_scaling_factor
+
 
     def request(self):
         _ts = parser.parse("2017-08-01 16:00:00")
@@ -53,9 +84,5 @@ class PeakManagementService():
 
         return forecast
 
-    def change_config(self):
-        fleet_config = FleetConfig(is_P_priority=True, is_autonomous=False, autonomous_threshold=0.1)
-        self.fleet.change_config(fleet_config)
-
-    def normalize_p(self, p):
-        return p
+    def run_fleet_test(self):
+        foreach day
