959db3b2acfdffcae01f2ee82614e9a62bdc3019
DavidRosewater <40434080+DavidRosewater@users.noreply.github.com>
2018-07-20 12:55:16 -0600

    Add files via upload

--- src/fleets/battery_inverter_fleet/test.py
+++ src/fleets/battery_inverter_fleet/test.py
@@ -7,29 +7,32 @@ import numpy
 from fleet_request import FleetRequest
 from fleet_response import FleetResponse
 from battery_inverter_fleet import BatteryInverterFleet
+from grid_info import GridInfo
 import matplotlib.pyplot as plt
 import scipy.io as spio
 import csv
 import copy
 
-def fleet_test(Fleet):
+def fleet_test(Fleet,Grid):
     
     mat = spio.loadmat('ERM_model_validation.mat', squeeze_me=True)
     t = mat['TT']
     P = -Fleet.num_of_devices* mat['PP']
     S = mat['SS']
 
-    n = len(t)
+    n = 8000#len(t)
     Pach = numpy.zeros((n))
+    Qach = numpy.zeros((n))
+    f = numpy.zeros((n,2))
+    v = numpy.zeros((n,2))
     SOC = numpy.zeros((n,Fleet.num_of_devices))
     #V = numpy.zeros(n)
 
     requests = []
     ts = datetime.utcnow()
-    
     dt = timedelta(hours=0.000092593) #hours
     for i in range(n):
-        req = FleetRequest(ts=ts,sim_step=dt,p=P[i],q=0.0)
+        req = FleetRequest(ts=(ts+i*dt),sim_step=dt,p=P[i],q=None)
         requests.append(req)
 
     """ # print the initial SoC
@@ -41,13 +44,22 @@ def fleet_test(Fleet):
     # print the forecasted achivable power schedule
     """ for i in range(n):
         rsp = FORCAST[i]
-        P[i] = rsp.P_injected """
+        P[i] = rsp.P_service """
 
     # process the requests 
     i = 0
     for req in requests:
-        Fleet.process_request(req)
-        Pach[i] = sum(Fleet.P_injected)
+        Fleet.process_request(req,Grid)
+        Pach[i] = sum(Fleet.P_service)
+        Qach[i] = sum(Fleet.Q_service)
+        f[i,0] = Grid.get_frequency(ts+i*dt,0)
+        f[i,1] = Grid.get_frequency(ts+i*dt,1)
+        v[i,0] = Grid.get_voltage(ts+i*dt,0)
+        v[i,1] = Grid.get_voltage(ts+i*dt,1)
+        """ if v[i] < 100:
+            print(str(100*i/n) + ' %') """
+        if numpy.mod(i,10000) == 0:
+            print(str(100*i/n) + ' %')
         for j in range(Fleet.num_of_devices):
             SOC[i,j] = Fleet.soc[j] # show that process_request function updates the SoC
         #V[i] = Fleet.vbat
@@ -58,21 +70,29 @@ def fleet_test(Fleet):
     plt.plot(t[0:n], P[0:n], label='Power Requested')
     plt.plot(t[0:n], Pach, label='Power Achieved by Fleet')
     plt.xlabel('Time (hours)')
-    plt.ylabel('Power')
+    plt.ylabel('Real Power (kW)')
     plt.legend(loc='lower right')
     plt.subplot(212)
-    plt.plot(t[0:n],SOC, label='Simulated SoC')
-    plt.plot(t[0:n],100*S[0:n], label='Recorded SoC')
+    plt.plot(t[0:n],f[0:n,0], label='Grid Frequency')
+    #plt.plot(t[0:n],100*S[0:n], label='Recorded SoC')
     plt.xlabel('Time (hours)')
-    plt.ylabel('State-of-Charge (%)')
+    plt.ylabel('frequency (Hz)')
     #plt.legend(loc='lower right')
 
-    """ plt.figure(2)
-    plt.plot(t, V) """
+    plt.figure(2)
+    plt.subplot(211)
+    plt.plot(t[0:n], Qach, label='Reactive Power Achieved by Fleet')
+    plt.ylabel('Reactive Power (kvar)')
+    plt.legend(loc='lower right')
+    plt.subplot(212)
+    plt.plot(t[0:n], v[0:n,0], label='Voltage at location 1')
+    plt.plot(t[0:n], v[0:n,1], label='Voltage at location 2')
+    plt.xlabel('Time (hours)')
+    plt.ylabel('Voltage (V)')
+    plt.legend(loc='lower right')
     plt.show()
 
 def integration_test(Fleet):
-
     # Establish the test variables
     n = 24
     del_t = timedelta(hours=1.0)
@@ -103,7 +123,7 @@ def integration_test(Fleet):
                 responses = Fleet.forecast(requests)
 
                 for T in numpy.arange(0, n):
-                    Power[T] = responses[T].P_injected
+                    Power[T] = responses[T].P_service
                 
                 SoCFin = responses[n-1].soc  # get final SoC
                 [P2, Cost, Able] = Fleet.cost(SoCFin, SoC0, del_t)  # retreeve how much power it would take to return to SoC0
@@ -125,8 +145,13 @@ def integration_test(Fleet):
 
 
 if __name__ == '__main__':
-    Fleet = BatteryInverterFleet('config_ERM.ini')
-    fleet_test(Fleet)
-    Fleet.soc = 50.0*numpy.ones(Fleet.num_of_devices)
-    integration_test(Fleet)
+    Fleet = BatteryInverterFleet('ERM')
+    location = 0
+    i = 0
+    Grid = GridInfo('Grid_Info_DATA_2.csv')
+    ts = datetime.utcnow()
+
+    fleet_test(Fleet,Grid)
+    #Fleet.soc = 50.0*numpy.ones(Fleet.num_of_devices)
+    #integration_test(Fleet)
 
