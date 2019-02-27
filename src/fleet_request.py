689a22441edbacd2c13f4b949b087d3b006960a6
Hayden-Reeve <46937467+Hayden-Reeve@users.noreply.github.com>
2019-02-27 12:21:51 -0800

    Update fleet_request.py

--- src/fleet_request.py
+++ src/fleet_request.py
@@ -12,12 +12,16 @@ class FleetRequest:
     """
     def __init__(self, ts=datetime.utcnow(),
                  sim_step=timedelta(hours=1),
+                 start_time=None,
                  p=None, q=None, steps=1):
         """
         Constructor
         """
         # Timestamp in simulation loop: datetime
         self.ts_req = ts
+        
+        # Timestamp in simulation loop: datetime
+        self.start_time = start_time
 
         # Simulation time step: timedelta object
         self.sim_step = sim_step
