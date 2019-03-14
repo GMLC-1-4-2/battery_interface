7423653d0f568017fbf80976b496264c3f799e6f
Robert Hosbach <rahosbach@lbl.gov>
2019-03-14 10:45:36 -0700

    Add calculations for and plotting of P_togrid and P_base for regulation service.

--- src/services/reg_service/reg_service.py
+++ src/services/reg_service/reg_service.py
@@ -136,13 +136,18 @@ class RegService():
         P_request = [r.P_req for r in request_list_2s_tot]
         ts_request = [r.ts_req for r in request_list_2s_tot]
         P_response = [r.P_service for r in response_list_2s_tot]
+        P_togrid = [r.P_togrid for r in response_list_2s_tot]
             
         # Save the responses to a csv
         results_df = pd.DataFrame({
             'DateTime': ts_request,
             'P_request': P_request,
-            'P_response': P_response
+            'P_response': P_response,
+            'P_togrid': P_togrid
             })
+        # Calculate P_base
+        results_df['P_base'] = results_df['P_togrid'] - results_df['P_response']
+        # Add SoC if battery fleet
         if 'battery' in fleet_name.lower():
             SOC = [r.soc for r in response_list_2s_tot]
             results_df['SOC'] = SOC
@@ -163,16 +168,23 @@ class RegService():
                         fleet_name + '.png'
         plt.figure(1)
         plt.figure(figsize=(15,8))
-        plt.subplot(211)
+        plt.subplot(311)
         plt.plot(ts_request, P_request, label='P request')
         plt.plot(ts_request, P_response, label='P response')
         plt.legend(loc='best')
         plt.ylabel('Power (MW)')
         if 'battery' in fleet_name.lower():
-            plt.subplot(212)
+            plt.subplot(312)
             plt.plot(ts_request, SOC)
             plt.ylabel('SOC (%)')
             plt.xlabel('Date and Time')
+        plt.subplot(313)
+        if (not(all(pd.isnull(results_df['P_togrid'])))):
+            plt.plot(ts_request, P_togrid, label='P_togrid')
+        if (not(all(pd.isnull(results_df['P_base'])))):
+            plt.plot(ts_request, results_df.P_base, label='P_base')
+        plt.ylabel('Power (MW)')
+        plt.legend(loc='best')
         plt.savefig(plot_dir + plot_filename, bbox_inches='tight')
         plt.close()      
 
