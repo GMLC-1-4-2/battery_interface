21583c08ba70cef9fb7842f784d466fe5dff933b
mayh819 <mayh819@WE34597.pnl.gov>
2019-03-28 09:22:29 -0500

    updates made for scaling service to fleet rating and plotting consistently

--- src/services/peak_managment_service/peak_management_service.py
+++ src/services/peak_managment_service/peak_management_service.py
@@ -6,10 +6,13 @@
 import sys
 from os.path import dirname, abspath, join
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
+import matplotlib.pyplot as plt
+
 
 from dateutil import parser
 from datetime import datetime, timedelta
 import pandas as pd
+import numpy as np
 import configparser
 
 import utils
@@ -18,13 +21,14 @@ from fleet_request import FleetRequest
 from fleet_response import FleetResponse
 from fleet_config import FleetConfig
 
+from utils import ensure_ddir
 
 class PeakManagementService:
     """
     The peak management service short summary
     """
 
-    def __init__(self, fleet, *args, **kwargs):
+    def __init__(self, fleet=None, *args, **kwargs):
         # The scope of the project is to test service with one fleet...
         self.fleet = fleet
 
@@ -67,49 +71,126 @@ class PeakManagementService:
 
         # Ideally the fleet could tell us its capacity and we compute the scaling_factor from that
         # and the maximum value in the drive cycle...
-        self.drive_cycle.load_forecast_mw /= self.capacity_scaling_factor
-
+        #self.drive_cycle.load_forecast_mw /= self.capacity_scaling_factor
+        '''
+        # normalizes drive cycle and scales to fleet service capacity
+        self.drive_cycle.load_forecast_mw *= self.fleet.assigned_service_kW()/max(self.drive_cycle.load_forecast_mw)
+        
         # Find the "annual" peak (largest peak in our drive cycle really) and the desired new peak...
+        #self.annual_peak = max(self.drive_cycle.load_forecast_mw)
         self.annual_peak = max(self.drive_cycle.load_forecast_mw)
         self.mw_target = self.annual_peak * (1 - self.f_reduction)
-
+        '''
         # TODO:  Initialize something to hold stats
 
-    def run_fleet_forecast_test(self):
+    def request_loop(self, start_time,fleet_name="PVInverterFleet"):
         cycle = 24
         ndx_start = 0
         ndx_end = ndx_start + cycle
+                # normalizes drive cycle and scales to fleet service capacity
+        self.drive_cycle.load_forecast_mw *= self.fleet.assigned_service_kW()/max(self.drive_cycle.load_forecast_mw)
+        
+        # Find the "annual" peak (largest peak in our drive cycle really) and the desired new peak...
+        #self.annual_peak = max(self.drive_cycle.load_forecast_mw)
+        self.annual_peak = max(self.drive_cycle.load_forecast_mw)
+        self.mw_target = self.annual_peak * (1 - self.f_reduction)
+        requests = []
+        responses = []
         while ndx_end < len(self.drive_cycle.dt):   # Loop over days...
             dt_day = self.drive_cycle.dt[ndx_start:ndx_end]
             dt_day.reset_index(drop=True, inplace=True)
             mw_day = self.drive_cycle.load_forecast_mw[ndx_start:ndx_end]
             mw_day.reset_index(drop=True, inplace=True)
             p_needed = [max(0, mw_day[i] - self.mw_target) for i in range(cycle)]
-
+            '''
+            for index, item in enumerate(p_needed):
+                if item == 0:
+                    p_needed[index] = None
+            '''
             ndx_start += 24
             ndx_end += 24
 
             # No need to work on days without high peaks
             if max(mw_day) <= self.mw_target:
-                continue
+                for i in range(24):
+                    fleet_request = FleetRequest(ts=dt_day[i], sim_step=self.sim_step, start_time=start_time, p=0)
+                    fleet_response = self.fleet.process_request(fleet_request)
+                # store performance stats
+                    requests.append(fleet_request)
+                    responses.append(fleet_response)
+                    print(responses[i].P_service)
+            else:
+                # Get a 24-hour forecast from fleet
+                forecast_requests = [FleetRequest(ts=dt_day[i], sim_step=self.sim_step, p=p_needed[i]) for i in range(cycle)]
+                forecast_response = self.fleet.forecast(forecast_requests)
+    
+                # See if the forecast can meet the desired load
+                deficit = [forecast_response[i].P_service - p_needed[i] for i in range(24)]
+                insufficient = [deficit[i] < 0 for i in range(cycle)]
+                if any(insufficient):
+                    # TODO:  NEED TO LOOP BACK AND REBUILD requests until we have a 24-hour request we know can be met
+                    pass
+    
+                # Now we know what the fleet can do, so ask it to do it
+                for i in range(24):
+                    fleet_request = FleetRequest(ts=dt_day[i], sim_step=self.sim_step, start_time=start_time, p=forecast_response[i].P_service)
+                    fleet_response = self.fleet.process_request(fleet_request)
+                    # TODO:  store performance stats
 
-            # Get a 24-hour forecast from fleet
-            requests = [FleetRequest(ts=dt_day[i], sim_step=self.sim_step, p=p_needed[i]) for i in range(cycle)]
-            forecast_response = self.fleet.forecast(requests)
+                    requests.append(fleet_request)
+                    responses.append(fleet_response)
+                    print(responses[i].P_service)
+                    
+        request_list_1h = [(r.ts_req, r.P_req / 1000) for r in requests]        
+        request_df_1h = pd.DataFrame(request_list_1h, columns=['Date_Time', 'Request'])
+        
+        if 'battery' in fleet_name.lower():
+            # Include battery SoC in response list for plotting purposes
+            response_list_1h = [(r.ts, r.P_service / 1000, r.P_togrid, r.soc) for r in responses]
+            response_df_1h = pd.DataFrame(response_list_1h, columns=['Date_Time', 'Response', 'P_togrid', 'SoC'])
+        else:
+            response_list_1h = [(r.ts, r.P_service / 1000, r.P_togrid) for r in responses]
+            response_df_1h = pd.DataFrame(response_list_1h, columns=['Date_Time', 'Response', 'P_togrid'])
 
-            # See if the forecast can meet the desired load
-            deficit = [forecast_response[i].P_service - p_needed[i] for i in range(24)]
-            insufficient = [deficit[i] < 0 for i in range(cycle)]
-            if any(insufficient):
-                # TODO:  NEED TO LOOP BACK AND REBUILD requests until we have a 24-hour request we know can be met
-                pass
+        # This merges/aligns the requests and responses dataframes based on their time stamp 
+        # into a single dataframe
+        df_1h = pd.merge(
+            left=request_df_1h,
+            right=response_df_1h,
+            how='left',
+            left_on='Date_Time',
+            right_on='Date_Time')
 
-            # Now we know what the fleet can do, so ask it to do it
-            for i in range(24):
-                fleet_request = FleetRequest(ts=dt_day[i], sim_step=self.sim_step, p=forecast_response[i].P_service)
-                fleet_response = self.fleet.process_request(fleet_request)
-                # TODO:  store performance stats
+        df_1h['P_togrid'] = df_1h['P_togrid'] / 1000
+        df_1h['P_base'] = df_1h['P_togrid'] - df_1h['Response']
 
+        # Plot entire analysis period results and save plot to file
+        # We want the plot to cover the entire df_1h dataframe
+        plot_dir = join(dirname(dirname(dirname(abspath(__file__)))), 'integration_test', 'peak_management_service')
+        ensure_ddir(plot_dir)
+        plot_filename = datetime.now().strftime('%Y%m%d') + '_all_' + start_time.strftime('%B') + '_events_' + fleet_name + '.png'
+        plt.figure(1)
+        plt.figure(figsize=(15, 8))
+        plt.subplot(211)
+        if not(all(pd.isnull(df_1h['Request']))):
+            plt.plot(df_1h.Date_Time, df_1h.Request, label='P_Request')
+        if not(all(pd.isnull(df_1h['Response']))):
+            plt.plot(df_1h.Date_Time, df_1h.Response, label='P_Response')
+        if not(all(pd.isnull(df_1h['P_togrid']))):
+            plt.plot(df_1h.Date_Time, df_1h.P_togrid, label='P_togrid')
+        if not(all(pd.isnull(df_1h['P_base']))):
+            plt.plot(df_1h.Date_Time, df_1h.P_base, label='P_base')
+        plt.ylabel('Power (MW)')
+        plt.legend(loc='best')
+        if ('battery' in fleet_name.lower()) & (not(all(pd.isnull(df_1h['SoC'])))):
+            plt.subplot(212)
+            plt.plot(df_1h.Date_Time, df_1h.SoC, label='SoC')
+            plt.ylabel('SoC (%)')
+            plt.xlabel('Time')
+        plt.savefig(join(plot_dir, plot_filename), bbox_inches='tight')
+        plt.close()
+       
+                             
     def process_stats(self):
         pass
         # TODO:  Aggregate up the fleet's performance stats and...do what?  Print them?  Write them to a file?
