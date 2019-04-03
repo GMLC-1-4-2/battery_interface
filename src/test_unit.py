1d310912a809c4e5f02d81672a22d587d42ab346
Hayden Reeve <46937467+Hayden-Reeve@users.noreply.github.com>
2019-04-03 15:15:41 -0700

    Update test_unit.py

--- src/test_unit.py
+++ src/test_unit.py
@@ -5,6 +5,10 @@ from fleet_request import FleetRequest
 from datetime import datetime, timedelta
 from fleet_factory import create_fleet
 
+# fleets = ['BatteryInverter', 'ElectricVehicle', 'PV', 'WaterHeater', 'Electrolyzer', 'FuelCell', 'HVAC', 'Refridge' ]
+fleet_name = 'Electrolyzer'
+
+#======================   DEVICE API TESTING   =============================
 
 class TestDeviceAPI(unittest.TestCase):
 # This class is specifically designed to test for the presense of device output consistent with the virtual battery API
@@ -13,27 +17,21 @@ class TestDeviceAPI(unittest.TestCase):
 
     def setUp(self):
     #This method instantiates the fleet and executes a simple request to get the device response for evaluation
-        service_name = 'DistributionVoltageService'
-    # fleets = ['BatteryInverter', 'ElectricVehicle', 'PV', 'WaterHeater', 'Electrolyzer', 'FuelCell', 'HVAC', 'Refridge' ]
-        fleet_name = 'PV'
-        kwargs = {}
+
         #start_time = parser.parse('8/1/17 16:00')
-        start_time = datetime.utcnow()
-        kwargs['start_time'] = start_time
-        cur_time = parser.parse('8/1/17 16:00')
+        #cur_time = parser.parse('8/1/17 16:00')
         #delt = timedelta(seconds=30)
-        #delt = timedelta(seconds=60)
+
+        start_time = datetime.utcnow()
         cur_time = datetime.utcnow()
         delt = timedelta(hours=0.000277777778)
+
         Prequest = 10;
-        Qrequest = 0;
+        Qrequest = None;
 
-        if service_name == 'ArtificialInertia':
-            kwargs['autonomous'] = 'autonomous'
-
+        kwargs = {}
+        kwargs['start_time'] = start_time
         grid_type = 1
-        if service_name == 'ArtificialInertia':
-            grid_type = 2
 
     # Create test fleet
         fleet = create_fleet(fleet_name, grid_type, **kwargs)
@@ -46,6 +44,9 @@ class TestDeviceAPI(unittest.TestCase):
 
     #Variables below are grid service request responses for the time period beginning at t0
     # and are returned to the high-level model
+
+    def test_P_base(self):
+        self.assertIsNotNone(self.response.P_base, 'P_base is not provided!')
 
     def test_P_togrid(self):
         self.assertIsNotNone(self.response.P_togrid, 'P_togrid is not provided!')
@@ -122,16 +123,62 @@ class TestDeviceAPI(unittest.TestCase):
     def test_SOC_cost(self):
         self.assertIsNotNone(self.response.SOC_cost, 'SOC_cost is not provided!')
 
-#   Test designed to fail -------
-#    def test_Fail(self):
-#        self.assertIsNotNone(self.response.P_Fail, 'P-Fail is not provided!')
+    def tearDown(self):
+        self.response = []
+
+#======================   DEVICE TIME MANAGEMENT TESTING   =============================
+
+class TestDeviceTime(unittest.TestCase):
+# This class is specifically designed to test for consistency and flexibility in handling timestampes and timesteps
+
+    def setUp(self):
+    #This method instantiates the fleet and executes a simple request to get the device response for evaluation
+
+        start_time = parser.parse('8/1/17 16:00')
+        #cur_time = parser.parse('8/1/17 16:00')
+        cur_time = datetime.utcnow()
+        delt = timedelta(seconds=0.5)
+
+        Prequest = 10;
+        Qrequest = None;
+
+        kwargs = {}
+        kwargs['start_time'] = start_time
+        grid_type = 1
+
+    # Create test fleet
+        fleet = create_fleet(fleet_name, grid_type, **kwargs)
+        if fleet is None:
+            raise 'Could not create fleet with name ' + fleet_name
+
+        fleet_request = FleetRequest(cur_time, delt, start_time, Prequest, Qrequest)
+        fleet_response = fleet.process_request(fleet_request)
+        self.response = fleet_response
+        self.cur_time = cur_time
+        self.delt = delt
+
+    #Variables below are grid service request responses for the time period beginning at t0
+    # and are returned to the high-level model
+
+    def test_TimeStamp(self):
+        self.assertEqual(self.response.ts, self.cur_time, 'Request and response timestamps do not match')
+
+    def test_TimeStep(self):
+        self.assertEqual(self.response.sim_step, self.delt, 'Request and response timesteps do not match')
+
+    def tearDown(self):
+        self.response = []
+
+
 
 if __name__ == '__main__':
     unittest.main()
 
 #Concepts for later tests:
 # Evaluate that P-Service is reasonable and of the correct sign.
-# Test to see correct start-time is used by device
+# Test to see that the devices can run with a range of P_request and Q_request
+# Entire test class for frequency response. - skip test for fleets not eligible.
+# Check that fleet passes scaling factor
 # Run to see that it can run with range of start times.
 # Run to see that it can run with range of time-steps.
-# Test that device is marching through time at the correct time step.
+# Investigate test class grouping to see if execution can be sped up by doing setup once per set of tests.
