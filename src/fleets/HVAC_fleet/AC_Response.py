db73cdbbe7afc88443d2febde05a6461a4f79926
Reeve <hayden.reeve@pnnl.gov>
2019-05-10 10:54:07 -0700

    HVAC

--- src/fleets/HVAC_fleet/AC_Response.py
+++ src/fleets/HVAC_fleet/AC_Response.py
@@ -20,12 +20,35 @@ class ACResponse:
         Constructor with default values
         """
         
+        self.TinB = 0
+        self.TwallB = 0
+        self.TmassB = 0
+        self.TatticB = 0
+        self.SOC_b = 0
+        self.ElementOnB = 0
+
         self.Tin = 0
         self.Twall = 0
         self.Tmass = 0
         self.Tattic = 0
+
+
         self.Tset = 0 
         self.Eused = 0 
+        self.Pbase = 0
+
+        self.lockoffB = 0
+        self.lockonB = 0 
+        self.lockoff = 0
+        self.lockon = 0
+        self.sim_step = 0
+
+        self.cycle_off_grid = 0
+        self.cycle_on_grid = 0
+
+        self.cycle_off_base = 0
+        self.cycle_on_base = 0
+
         self.PusedMax = 0
         self.PusedMin = 0
         self.ElementOn = 0
